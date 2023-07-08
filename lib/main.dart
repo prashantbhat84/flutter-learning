ac44ff93640fe737633e8c0e5d57d14c5e055cde
Prashant Bhat <prashantbhat84@gmail.com>
2023-07-08 21:21:00 +0530

    added custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: Center(
-        child: Text('My first flutter application'),
-      ),
+      // backgroundColor: Color.fromARGB(255, 103, 173, 134),
+      body: GradientContainer(),
     ),
   ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+        Color.fromARGB(255, 149, 55, 55),
+        Color.fromARGB(137, 47, 150, 124)
+      ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+      child: const Center(
+        child: Text('Hello world',
+            style: TextStyle(color: Colors.white70, fontSize: 28)),
+      ),
+    );
+  }
 }
